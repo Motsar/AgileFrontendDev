833dcce9217842104b6a9809fab8125b03ada07d
martin.motsar <martin.motsar@khk.ee>
2021-05-06 02:47:33 +0300

    API client created

---
+++ src/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://outside-in-dev-api.herokuapp.com/QsBeIGASD0FGWOc4oGGHAsiVERrqtYf5',
+});
+
+const api = {
+  loadRestaurants() {
+    return client.get('/restaurants').then(response => response.data);
+  },
+};
+
+export default api;
